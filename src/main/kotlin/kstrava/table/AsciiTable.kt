d5bcd091886dfb3669b57d5dc362486e49819f7c
mgryszko <marcin.gryszko@lastminute.com>
2020-05-18 08:31:50 +0200

    Abstract column fitting for single row with Foldable and Monoid typeclasses

--- src/main/kotlin/kstrava/table/AsciiTable.kt
+++ src/main/kotlin/kstrava/table/AsciiTable.kt
@@ -1,5 +1,15 @@
 package com.grysz.kstrava.table
 
+import arrow.Kind
+import arrow.core.ListK
+import arrow.core.extensions.listk.applicative.applicative
+import arrow.core.extensions.listk.foldable.foldable
+import arrow.core.extensions.listk.monoidK.monoidK
+import arrow.core.fix
+import arrow.core.k
+import arrow.typeclasses.Applicative
+import arrow.typeclasses.Foldable
+import arrow.typeclasses.MonoidK
 import arrow.typeclasses.Show
 import com.grysz.kstrava.table.Align.LEFT
 
@@ -28,11 +38,35 @@ data class Table<in A>(val columns: List<MinWidthColumn>, val renderers: List<Sh
 
 fun <A> Table<A>.fitContent(values: List<A>): Table<A> {
     val newColumns = values.fold(columns) { newColumns, value ->
-        newColumns.zip(renderers).fold(emptyList()) { acc: List<MinWidthColumn>, (column: MinWidthColumn, renderer: Show<A>) ->
-            acc + column.fitTo(with(renderer) { value.show().length })
-        }
+        val columnsWithRenderers: List<Pair<MinWidthColumn, Show<A>>> = newColumns.zip(renderers)
+        fitToRenderedWidths(
+            FD = ListK.foldable(),
+            MN = ListK.monoidK(),
+            AP = ListK.applicative(),
+            columnsWithRenderers = columnsWithRenderers.k(),
+            value = value
+        ).fix()
     }
     return copy(columns = newColumns, renderers = renderers)
 }
 
+private fun <F, A> fitToRenderedWidths(
+    FD: Foldable<F>,
+    MN: MonoidK<F>,
+    AP: Applicative<F>,
+    columnsWithRenderers: Kind<F, Pair<MinWidthColumn, Show<A>>>,
+    value: A
+): Kind<F, MinWidthColumn> = FD.run { AP.run {
+    columnsWithRenderers.foldMap(MN.algebra()) { (column: MinWidthColumn, renderer: Show<A>) ->
+        fitToRenderedWidth(column, renderer, value).just()
+    }
+} }
+
+private fun <A> fitToRenderedWidth(
+    column: MinWidthColumn,
+    renderer: Show<A>,
+    value: A
+): MinWidthColumn = column.fitTo(with(renderer) { value.show().length })
+
 private fun MinWidthColumn.fitTo(newWidth: Int): MinWidthColumn = if (newWidth > width) copy(width = newWidth) else this
+
