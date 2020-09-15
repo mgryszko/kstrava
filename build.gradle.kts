8d1b570d37d0efa07868e04b01d1b7c8b0483e47
mgryszko <marcin.gryszko@lastminute.com>
2020-09-15 07:02:05 +0200

    Adjust Kotlin compilation options to latest Gradle and Kotlin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -51,18 +52,13 @@ dependencies {
     testImplementation("io.mockk:mockk:${Versions.mockk}")
 }
 
+val kotlinOptions: KotlinJvmOptions.() -> Unit = {
+    jvmTarget = "11"
+}
 val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    languageVersion = "1.4"
-    jvmTarget = "11"
-    freeCompilerArgs += listOf("-Xnew-inference")
-}
-
+compileKotlin.kotlinOptions(kotlinOptions)
 val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    languageVersion = "1.4"
-    jvmTarget = "11"
-}
+compileTestKotlin.kotlinOptions(kotlinOptions)
 
 tasks.test {
     useJUnitPlatform()
